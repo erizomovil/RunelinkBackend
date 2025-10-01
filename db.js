beecdf8ee75bc0e0428dba35a5ecffe001a15fb2
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-01 16:09:29 +0100

    cros fixed 13

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync('./ca.crt').toString(), // ruta al certificado Aiven
+    ca: fs.readFileSync('./ca.pem').toString(), // ruta al certificado Aiven
   },
 });
 
