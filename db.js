d7a29387c5af802be0b319dbedee127bcb74d0b1
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-01 16:18:18 +0100

    cros fixed 14

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-const fs = require('fs');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    ca: fs.readFileSync('./ca.pem').toString(), // ruta al certificado Aiven
-  },
+  ssl: true,
 });
 
 module.exports = pool;
