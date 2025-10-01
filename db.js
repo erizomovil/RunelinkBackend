5110fceef5a5a454d9e5e7074c54a43e57d555d2
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-01 15:59:11 +0100

    cros fixed 11

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // ⚡ importante si accedes desde fuera de Render
+    ca: fs.readFileSync('./ca.crt').toString(), // ruta al certificado Aiven
   },
 });
 
