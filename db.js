df29d458a33a76bf1c113fe003ea21a56fc2eef0
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-20 13:11:09 +0100

    ca fixed5

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-import pkg from "pg";
-const { Pool } = pkg;
+const fs = require("fs");
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Neon usa SSL
+    rejectUnauthorized: false,
   },
 });
 
