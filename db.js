4ba79809352a2fafcc9ec385e300a82cb0f120e0
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-20 13:00:04 +0100

    ca fixed4

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
-const fs = require("fs");
-const { Pool } = require('pg');
-
-const caPath = "./ca.pem"; 
-console.log("📜 Leyendo certificado de:", caPath);
-console.log("Certificado (primeras 100 chars):", fs.readFileSync(caPath).toString().slice(0, 100));
+import pkg from "pg";
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync(caPath).toString(),
+    rejectUnauthorized: false, // Neon usa SSL
   },
 });
 
