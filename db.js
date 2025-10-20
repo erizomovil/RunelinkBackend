3363bf5e11c1f6311576896007ae7968325c052f
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-20 12:30:16 +0100

    ca fixed2

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const { Pool } = require('pg');
-require('dotenv').config();
+
+const caPath = "/etc/secrets/ca.pem"; // 👈 o "./ca.pem" según tu caso
+console.log("📜 Leyendo certificado de:", caPath);
+console.log("Certificado (primeras 100 chars):", fs.readFileSync(caPath).toString().slice(0, 100));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync("./ca.pem").toString(),
+    ca: fs.readFileSync(caPath).toString(),
   },
 });
 
