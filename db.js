0d4706eba6405103478ec6cac3ccb3d8b93a470d
erizomovil <jg.arcalagonzalez@gmail.com>
2025-10-20 12:10:42 +0100

    ca

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    ca: fs.readFileSync("./ca.pem").toString(),
+  },
 });
 
 module.exports = pool;
